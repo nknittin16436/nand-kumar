bcfb2908c01914fe529a094e5c21abdeeaf0a00b
Nand Kumar <nand.kumar@bitcs.in>
2022-08-31 13:13:57 +0530

    Fixed some icon delte

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -40,7 +40,7 @@ class Header extends Component {
           </div>
         </div>
         <p className="scrolldown">
-          <a className="smoothscroll" href="#about" rel="noopener noreferrer"><i className="icon-down-circle"></i></a>
+          <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
 
         </p>
 
